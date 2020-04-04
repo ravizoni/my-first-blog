f3120abe923b975035562b084ffed8b680e14deb
Raphael Vizoni <raphael.vizoni@olist.com>
2020-04-04 01:24:56 -0300

    Editing post with form template

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -11,3 +12,35 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    #vai pegar a classe la do forms.py, q define os campos
+    if request.method == "POST":
+        form = PostForm(request.POST) # atribui os valores do formulario na variavel post
+        if form.is_valid():
+            # não salva o Post no model ainda: falta adicionar autor, published_date
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.created_date = timezone.now()
+            post.save()
+            #redireciona
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
